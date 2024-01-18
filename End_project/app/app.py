cf6838168bc52e5f22b4a2714d8c695fc7a4543c
Karol <socer404@gmail.com>
2024-01-18 22:37:35 +0100

    Refactor TOTP, fix bug with flash messages

--- End_project/app/app.py
+++ End_project/app/app.py
@@ -14,7 +14,7 @@ from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 
@@ -28,6 +28,7 @@ Bootstrap5(app)
 limiter = Limiter(get_remote_address, app=app, default_limits=["200 per day", "50 per hour"])
 # remote address is the IP address of the user accessing the website.
 # default_limits is a list of rules that apply to all routes.
+# TODO : add in memory cache to reduce load on database
 
 # Configure Flask-Login
 login_manager = LoginManager()
@@ -107,21 +108,21 @@ with app.app_context():
 @monitor_system
 def register():
 
-    form = RegisterForm()
-    if form.validate_on_submit():
+    register_form = RegisterForm()
+    if register_form.validate_on_submit():
         # Check if user email is already present in the database.
-        result = db.session.execute(db.select(User).where(User.email == form.email.data))
+        result = db.session.execute(db.select(User).where(User.email == register_form.email.data))
         user = result.scalar()
         if user:
             # User already exists
             flash("You've already signed up with that email, log in instead!")
             return redirect(url_for('login'))
         # Hash the password
-        salt_and_hash_password = hash_password(form.password.data, salt_length=8).split("$")
+        salt_and_hash_password = hash_password(register_form.password.data, salt_length=8).split("$")
         key = pyotp.random_base32()
         new_user = User(
-            email=form.email.data,
-            name=form.name.data,
+            email=register_form.email.data,
+            name=register_form.name.data,
             password=salt_and_hash_password[1],
             salt=salt_and_hash_password[0],
             totp_secret=key
@@ -130,39 +131,35 @@ def register():
         db.session.add(new_user)
         db.session.commit()
 
-        # Refactor
-
         # This line will authenticate the user with Flask-login
-        # login_user(new_user)
+        login_user(new_user)
         # return redirect(url_for("get_all_notes"))
-        return redirect(url_for("totp", user_id=new_user.id))
-    return render_template("register.html", form=form, current_user=current_user)
+        return redirect(url_for("totp"))
+    return render_template("register.html", form=register_form, current_user=current_user)
 
 
 @app.route('/totp', methods=['GET', 'POST'])
 @limiter.limit("5/minute")
+@login_required
 def totp():
-    user_id = request.args.get("user_id")
+
     # To register a new user, we need to generate a secret key and a QR code
     # This secret key will be used to generate OTPs
     # This QR code will be used to register the OTP with an authenticator app
     # After verifying the OTP, the user will be registered
     form = TOTPForm()
+    user = current_user
     if form.validate_on_submit():
-        user = db.get_or_404(User, user_id)
-
         if pyotp.TOTP(user.totp_secret).verify(form.token.data):
-            login_user(user)
             os.remove("app/static/qr.png")
             return redirect(url_for("get_all_notes"))
         else:
             flash("Incorrect OTP, please try again.")
-            return redirect(url_for("totp", user_id=user.id))
-    user = db.get_or_404(User, user_id)
+            return redirect(url_for("totp"))
+
     key = user.totp_secret
     uri = pyotp.totp.TOTP(key).provisioning_uri(name=user.name, issuer_name="Secure Notes App")
     qr = qrcode.make(uri).save("app/static/qr.png")
-
     return render_template("2FA.html", form=form)
 
 
@@ -175,7 +172,6 @@ def login():
         password = form.password.data
         result = db.session.execute(db.select(User).where(User.email == form.email.data))
         # email in db is unique so will only have one result.
-
         user = result.scalar()
 
         # User doesn't exist
@@ -188,15 +184,15 @@ def login():
             flash('Password incorrect, please try again.')
             return redirect(url_for('login'))
         else:
-            # login_user(user)
-            # return redirect(url_for('get_all_notes'))
-            return redirect(url_for("totp", user_id=user.id))
+            login_user(user)
+            return redirect(url_for("totp"))
 
     return render_template("login.html", form=form, current_user=current_user)
 
 
 # All routes below have default rate limits applied to them.
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('get_all_notes'))
@@ -219,7 +215,11 @@ def add_new_note():
     form = CreateNoteForm()
     if form.validate_on_submit():
         if form.encrypted.data:
-            encrypted_note, iv = encrypt(form.body.data, form.password.data)
+            if len(form.password.data) != 16:
+                flash("Please enter a 16 characters password to encrypt your note.")
+                return redirect(url_for("add_new_note"))
+            else:
+                encrypted_note, iv = encrypt(form.body.data, form.password.data)
 
             new_note = Note(
                 title=form.title.data,
@@ -237,7 +237,6 @@ def add_new_note():
                 author=current_user,
                 date=date.today().strftime("%B %d, %Y"),
                 encrypted=form.encrypted.data,
-
             )
         db.session.add(new_note)
         db.session.commit()
@@ -265,6 +264,7 @@ def show_note(note_id):
             else:
                 flash("Incorrect password, please try again.")
                 return redirect(url_for("show_note", note_id=note_id))
+        return render_template("note.html", note=requested_note, current_user=current_user, form=form)
 
     return render_template("note.html", note=requested_note, current_user=current_user)
 
